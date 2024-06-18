20cd56f5883dda51309bbe11dff43bc1d49c9fbb
acornies <andrew.cornies@hashicorp.com>
2024-06-17 21:36:21 -0400

    Fix borked outputs

--- outputs.tf
+++ outputs.tf
@@ -5,12 +5,3 @@ output "vault_address" {
 output "vault_namespaces" {
   value = module.vault_namespaces.vault_namespaces
 }
-
-output "terraform_workspaces" {
-  value = module.hcp_terraform_setup.workspaces
-}
-
-output "terraform_workspace_tokens" {
-  value     = module.hcp_terraform_setup.team_tokens
-  sensitive = true
-}
