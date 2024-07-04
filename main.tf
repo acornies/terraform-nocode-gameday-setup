a9672910c875896bcabc65469b85038f336451eb
acornies <andrew.cornies@hashicorp.com>
2024-07-03 20:51:57 -0400

    Bump aws module version to v0.1.3

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "vault_namespaces" {
 
 module "aws_central" {
   source  = "app.terraform.io/ender-corp/gameday-setup-aws/aws"
-  version = "0.1.2"
+  version = "0.1.3"
   # insert required variables here
   event_name               = var.event_name
   region                   = var.region
